4e3f89dc2b995e73df95bbe958942abe1a38e6e9
niraj mohate <nirajmohate@gmail.com>
2024-12-06 11:21:03 +0530

    Tic Tac Toe

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import TicTacToe from './components/Tic Tac Toe/TicTacToe'
+
+function App() {
+  
+
+  return (
+    <>
+      <TicTacToe/>
+    </>
+  )
+}
+
+export default App
